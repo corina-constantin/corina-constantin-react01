442413b9165675d15e4f802782ab1b74b651abcf
corina-constantin <corina.pescaru@gmail.com>
2022-12-17 16:11:26 +0200

    revert

--- app-contact-manager/modules/message.js
+++ app-contact-manager/modules/message.js
@@ -2,17 +2,7 @@ export default (message = '', type = 'success') => {
   const messageContainer = document.createElement('div');
   messageContainer.classList.add('alert', `alert-${type}`);
 
-  //x button
-  const closeButton = document.createElement('button');
-  closeButton.classList.add('btn', 'btn-close');
-
   messageContainer.innerText = message;
-  messageContainer.appendChild(closeButton);
-
-  //am pus 5 secunde ca nu apucam sa apas X pana disparea mesajul
-  setTimeout(() => {
-    messageContainer.remove();
-  }, 5000);
 
   return messageContainer;
 };
--- app-contact-manager/modules/notificationBar.js
+++ app-contact-manager/modules/notificationBar.js
@@ -8,14 +8,4 @@ export const clearMessages = () => {
   notificationBar.innerHTML = '';
 };
 
-notificationBar.addEventListener('click', (event) => {
-  const { target } = event;
-
-  if (target.nodeName !== 'BUTTON' || !target.classList.contains('btn-close')) {
-    return;
-  }
-
-  target.parentElement.remove();
-});
-
 export default notificationBar;
